f379093310de15f64fa8422765a99bd876820341
Danil M <antonovdania482@gmail.com>
2025-04-15 17:36:51 +0300

    Made async db creation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from routers import users, notes, analytics
-from db import models
-from db.database import engine
 from auth import authentication
+import asyncio
+from db.database import Base, engine
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 app = FastAPI()
 
@@ -11,4 +12,11 @@ app.include_router(notes.router)
 app.include_router(analytics.router)
 app.include_router(authentication.router)
 
-models.Base.metadata.create_all(engine)
+
+async def init_models(engine:AsyncEngine):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+if __name__ == "__main__":
+    asyncio.run(init_models(engine))
